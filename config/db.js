79f33c798e77d70e4294d846a98cc2442264f67a
Rafael Cruz <rafac@Rafaels-MacBook-Pro.local>
2018-10-17 16:38:35 -0400

    edited consoleLog

--- config/db.js
+++ config/db.js
@@ -1,9 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 //Map Global Promises
 mongoose.Promise = global.Promise;
 
 //Mongoose Connect.
-mongoose.connect('mongodb://ralph:rc042101@ds251240.mlab.com:51240/pollingapp')
-        .then(() => console.log('MongoDb Connected'))
-        .catch(err => console.log(err));
+mongoose
+  .connect("mongodb://ralph:rc042101@ds251240.mlab.com:51240/pollingapp")
+  .then(() => console.log("Connected to Mlab."))
+  .catch(err => console.log(err));
